89456b490ece24c9434e6204a8072b967abe6157
saniazafar <saniazafarabbas@gmail.com>
2020-04-01 19:50:05 +0400

    Initial Commit

---
+++ MLKitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLKitDemo
+//
+//  Created by sania zafar on 01/04/2020.
+//  Copyright © 2020 sania zafar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
